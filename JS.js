0c174f781c219b876dce3a3071e396a5eddf0658
Borja <riosyharim@yahoo.com>
2023-08-27 18:03:39 -0300

    first commit

---
+++ JS.js
@@ -0,0 +1,12 @@
+
+var nombre = "Borja";
+var apellido = "Rios";
+console.log(nombre + apellido);
+
+var numero1 = 5;
+var numero2 = 10;
+var suma = numero1 + numero2;
+console.log("La suma es:", suma);
+var otroNumero = 3;
+var resultadoFinal = suma + otroNumero;
+console.log("El resultado final es:", resultadoFinal);
